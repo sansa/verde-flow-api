a87156a7bcba420dae3842b1aaf978c4f96b752f
mbanyick <73003805+mbanyick@users.noreply.github.com>
2025-06-07 08:31:22 +0300

    chore: enable CORS for Swagger and cross-origin requests

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger.config.js";
@@ -9,6 +10,7 @@ import errorHandler from "./middleware/error.handler.js";
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
